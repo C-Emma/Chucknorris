ef00b9a4e7c4c7cdbbd78689f76bf810b06169ae
Emma.C <chenemma@outlook.fr>
2020-03-12 10:13:56 +0100

    Add RecyclerView in MainActivity

--- app/src/main/java/com/example/chucknorris/Jokes.kt
+++ app/src/main/java/com/example/chucknorris/Jokes.kt
@@ -1,2 +1,5 @@
 package com.example.chucknorris
 
+object JokesList {
+    val jokes = listOf("j1", "j2", "j3", "j4")
+}
--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.chucknorris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+private val LOGTAG = "Activity"
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(LOGTAG, JokesList.jokes.toString())
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = JokeAdapter(JokesList.jokes)
+
+        recyclerView = findViewById<RecyclerView>(R.id.id_recyclerView).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
+
 }
