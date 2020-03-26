298bc131706de45a59b8669cf61255ea74d84f72
Emma.C <chenemma@outlook.fr>
2020-03-26 10:17:03 +0100

    Remplacé TextView de JokeViewHolder par le View du joke_layout.xml

--- app/src/main/java/com/example/chucknorris/JokeAdapter.kt
+++ app/src/main/java/com/example/chucknorris/JokeAdapter.kt
@@ -1,10 +1,10 @@
 package com.example.chucknorris
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import org.w3c.dom.Text
 
 class JokeAdapter(var jokes:List<String> = emptyList()):
     RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
@@ -14,14 +14,14 @@ class JokeAdapter(var jokes:List<String> = emptyList()):
             notifyDataSetChanged()
         }
 
-    class JokeViewHolder(val textView: TextView) :
-        RecyclerView.ViewHolder(textView){
-        val jokesView = textView.findViewById<TextView>(R.id.jokes_view)
+    class JokeViewHolder(v : View) :
+        RecyclerView.ViewHolder(v){
+        val jokesView = v.findViewById<TextView>(R.id.jokes_view)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeAdapter.JokeViewHolder {
         val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.jokes_view, parent, false) as TextView
+            .inflate(R.layout.joke_layout, parent, false) as TextView
         return JokeViewHolder(textView)
     }
 
