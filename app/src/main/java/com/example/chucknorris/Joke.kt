d5e4bc41d0d92dc3c50f1000bba65f7b530fd935
Emma.C <chenemma@outlook.fr>
2020-04-19 22:33:36 +0200

    Test de JokeSerializationTest.kt (serialize() et deserialize() toujours infonctionnelle dans Joke.kt)

--- app/src/main/java/com/example/chucknorris/Joke.kt
+++ app/src/main/java/com/example/chucknorris/Joke.kt
@@ -1,21 +1,45 @@
 package com.example.chucknorris
 
 import kotlinx.serialization.*
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 
 @Serializable
-data class Joke (
+data class Joke(
+    @SerialName("categories")
+    val categories: List<String>,
     @SerialName("createdAt")
-    val createdAt: String = "2020-01-05 13:42:26.766831",
+    val createdAt: String,
     @SerialName("iconUrl")
-    val iconUrl: String = "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    val iconUrl: String,
     @SerialName("id")
-    val id: String = "pyNXTV7WThiNLRykGsQmrg",
+    val id: String,
     @SerialName("updatedAt")
-    val updatedAt: String = "2020-01-05 13:42:26.766831",
+    val updatedAt: String,
     @SerialName("url")
-    val url: String = "https://api.chucknorris.io/jokes/pyNXTV7WThiNLRykGsQmrg",
+    val url: String,
     @SerialName("value")
-    val value: String = "The hills are alive with the sound of Chuck Norris' dong slapping against his legs while he walks."
+    val value: String
 )
+    /*companion object : KSerializer<Joke>{
+        override val descriptor: SerialDescriptor = PrimitiveDescriptor("Joke", PrimitiveKind.STRING)
 
+
+        val data = Joke(
+        emptyList(),
+        "2020-01-05 13:42:26.766831",
+        "\"https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+        "pyNXTV7WThiNLRykGsQmrg",
+        "2020-01-05 13:42:26.766831",
+        "https://api.chucknorris.io/jokes/pyNXTV7WThiNLRykGsQmrg",
+        "The hills are alive with the sound of Chuck Norris' dong slapping against his legs while he walks."
+    )
+
+     fun serialize() {
+        val json = Json(JsonConfiguration.Stable).stringify(Joke.serializer(), data)
+         print(json)
+    }
+
+     fun deserialize() {
+        val joke = Json(JsonConfiguration.Stable).parse(Joke.serializer(), data)
+    }*/
